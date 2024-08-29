776e3cd8ac27b499effcafde602cceaa73faad59
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2024-08-30 01:19:09 +0530

    team page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Team from './pages/team';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Team />} />
+        
       </Routes>
     </Router>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './Navbar.css'; 
-import logo from '../../assets/logo.png'
+import React from 'react'; 
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 
 function Navbar() {
   return (
-    <nav>
-      <img src={logo}></img>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Team</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Techboard</a></li>
-        <li><a href="#">FAQs</a></li>
+    <nav className="bg-rose-500 p-4">
+      <img src={logo} alt="Logo" className="h-10 inline-block" />
+      <ul className="flex space-x-4 text-white">
+        <li><a href="#" className="hover:text-gray-200">Home</a></li>
+        <li><a href="#" className="hover:text-gray-200">Team</a></li>
+        <li><a href="#" className="hover:text-gray-200">Events</a></li>
+        <li><a href="#" className="hover:text-gray-200">Techboard</a></li>
+        <li><a href="#" className="hover:text-gray-200">FAQs</a></li>
       </ul>
     </nav>
   );
