3049ff37ffef231cd602c8f89322587aa1051a03
Vidushi Agarwal <vidushiagg2004@gmail.com>
2024-08-30 11:31:25 +0530

    Revert "Event Page Added"

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar/Navbar';
 import Mobile from '../components/MobileNavbar/mobile'; 
 import Footer from '../components/Footer/Footer'; 
 import Homepage from '../components/Home/homepage'; 
+
 const Home = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
@@ -19,7 +20,6 @@ const Home = () => {
     <div>
       {isMobile ? <Mobile /> : <Navbar />}
       <Homepage/>
-      <Navbar />
       <Footer />
     </div>
   );
